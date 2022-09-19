ae9e3f65070d5fdcd7d6e80307275110316d0bf9
MartinPrograms <36630572+MartinPrograms@users.noreply.github.com>
2022-09-19 09:42:23 +0200

    1 file modified

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,39 @@
 <head>
 	<style>
-	p{
-		
-	}
+		:root {
+ 	  --background: #e9dcdc;
+  	--white: #ffffff;
+		}
+
+
+		body{
+  	  background: var(--background);
+    	background-size: 400% 400%;
+	    size: 10vh;
+		}
+		.div{
+			display: flex;
+  		justify-content: center;
+  		align-items: center;
+  		text-align: center;
+  		min-height: 100vh;
+		}
+		button{
+			background: var(--background);
+			border-radius: 50px;
+			border-width: 4px;
+			min-width: 150px;
+			min-height: 50px
+		}
 	</style>
 	<title>Home</title>
 </head>
-<body>
+<body class="background">
+<div class="div">
 <button on:click={Button}>
 Show Alert
 </button>
+</div>
 </body>
 
 
